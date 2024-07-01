fcda44c277e8fe041daa129ef65eb97f5b78c83a
Kevin Sangani <kevinsangani99247@gmail.com>
2024-07-01 14:20:29 +0530

    Basic Template of the css is added

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <form action="">
+        <input
+          type="file"
+          className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
